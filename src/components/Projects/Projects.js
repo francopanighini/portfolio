d349c2fd9746098eb6af24e9a5bbabbd68502c59
fpanighini <fpanighini@itba.edu.ar>
2024-10-10 15:04:36 -0300

    More frontend improvements

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,36 +1,27 @@
-import React,{ useContext} from 'react';
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
 import { ThemeContext } from '../../contexts/ThemeContext';
 import { projectsData } from '../../data/projectsData'
-import { HiArrowRight } from "react-icons/hi";
 import ProjectCard from "./ProjectCard";
 
-import cryptuki from '../../assets/png/cryptuki.png'
-import yellow from '../../assets/jpg/yellow.jpg' 
-
-
-
 import './Projects.css'
-import SingleProject from './SingleProject/SingleProject';
 
 function Projects() {
 
     const { theme } = useContext(ThemeContext);
-    
+
     const useStyles = makeStyles(() => ({
-        viewAllBtn : {
-            color: theme.tertiary, 
+        viewAllBtn: {
+            color: theme.tertiary,
             backgroundColor: theme.primary,
             transition: 'color 0.2s',
             "&:hover": {
-                color: theme.secondary, 
+                color: theme.secondary,
                 backgroundColor: theme.primary,
             }
         },
-        viewArr : {
-            color: theme.tertiary, 
+        viewArr: {
+            color: theme.tertiary,
             backgroundColor: theme.secondary70,
             width: '40px',
             height: '40px',
@@ -40,27 +31,25 @@ function Projects() {
             cursor: 'pointer',
             transition: 'background-color 0.2s',
             "&:hover": {
-                color: theme.tertiary, 
+                color: theme.tertiary,
                 backgroundColor: theme.secondary,
             }
         },
     }));
 
-    const classes = useStyles();
-
     return (
         <>
             {projectsData.length > 0 && (
-                <div className="projects" id="projects" style={{backgroundColor: theme.secondary}}>
+                <div className="projects" id="projects" style={{ backgroundColor: theme.secondary }}>
                     <div className="projects--header">
-                        <h1 style={{color: theme.primary}}>Projects</h1>
+                        <h1 style={{ color: theme.primary }}>Projects</h1>
                     </div>
                     <div className="projects--body">
                         <div className="projects--bodyContainer">
                             {projectsData.map(project => (
-                                <ProjectCard project={project}/>
+                                <ProjectCard project={project} />
                             ))}
-                        </div> 
+                        </div>
                     </div>
                 </div>
             )}
