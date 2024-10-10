2087afc73f4e69228e58d04d0df76f44acc47f50
fpanighini <fpanighini@itba.edu.ar>
2024-10-10 15:03:48 -0300

    Improved styling and resizing of the webpage

--- src/components/Education/EducationCard.js
+++ src/components/Education/EducationCard.js
@@ -8,15 +8,16 @@ import eduImgWhite from '../../assets/svg/education/eduImgWhite.svg'
 import eduImgBlack from '../../assets/svg/education/eduImgBlack.svg'
 import './Education.css'
 
+
 function EducationCard({ id, institution, course, startYear, endYear }) {
 
     const { theme } = useContext(ThemeContext);
 
     const useStyles = makeStyles((t) => ({
-        educationCard : {
-            backgroundColor:theme.primary30,
+        educationCard: {
+            backgroundColor: theme.primary30,
             "&:hover": {
-                backgroundColor:theme.primary50,
+                backgroundColor: theme.primary50,
             },
         },
     }));
@@ -26,7 +27,7 @@ function EducationCard({ id, institution, course, startYear, endYear }) {
     return (
         <Fade bottom>
             <div key={id} className={`education-card ${classes.educationCard}`} >
-                <div className="educard-img" style={{backgroundColor: theme.primary}}>
+                <div className="educard-img" style={{ backgroundColor: theme.primary }}>
                     <img src={theme.type === 'light' ? eduImgBlack : eduImgWhite} alt="" />
                 </div>
                 <div className="education-details">
@@ -35,7 +36,7 @@ function EducationCard({ id, institution, course, startYear, endYear }) {
                     <h5 style={{color: theme.tertiary80}}>{institution}</h5>
                 </div>
             </div>
-        </Fade>        
+        </Fade>
     )
 }
 
